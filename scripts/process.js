9d0405a813e831f7245a590ccae43d7dfd5b32f0
Sorat Gessakorn <soratgessakorn@Sorats-MacBook-Pro.local>
2025-03-07 16:34:26 +0700

    update

--- scripts/process.js
+++ scripts/process.js
@@ -16,6 +16,7 @@ const da_wallet_address = '0xD387ad5Ea23De2CaF7493992BF60866c16aE3F5D'
 const fiat_quantity = '-'
 const da_asset_isin = '-'
 const customer_code_amlo = '-'
+var countries,nationalities,titles
 
 
 /**
@@ -126,6 +127,21 @@ async function getCustomerData() {
   return await readCSV(customerFile, "|"); // Read as pipe-delimited
 }
 
+async function getCountryMapping() {
+  const file = "DA-Master/DA_T_Mas_Country.csv";
+  return await readCSV(file, "|"); // Read as pipe-delimited
+}
+
+async function getNationalityMapping() {
+  const file = "DA-Master/DA_T_Mas_Nationality.csv";
+  return await readCSV(file, "|"); // Read as pipe-delimited
+}
+
+async function getTitleMapping() {
+  const file = "DA-Master/DA_T_Mas_Title.csv";
+  return await readCSV(file, "|"); // Read as pipe-delimited
+}
+
 /**
  * Generates a data file based on a given template.
  * @param {string} templateFileName - The template filename with placeholders.
@@ -134,6 +150,9 @@ async function getCustomerData() {
  * @param {number} yyyymmdd - Date in YYYYMMDD format.
  */
 export async function generateData(templateFileName, dbdNo, assetId, yyyymmdd) {
+  countries = await getCountryMapping();
+   nationalities = await getNationalityMapping();
+   titles = await getTitleMapping();
   const customers = await getCustomerData();
   const templateFilePath = path.join("DA-template", templateFileName);
   const fields = await readTemplateFields(templateFilePath);
@@ -168,8 +187,63 @@ export async function generateData(templateFileName, dbdNo, assetId, yyyymmdd) {
 
 function processCusData(customers, fields) {
   const processedData = customers.map(customer =>
-    fields.map(field => customer[field] || "").join("|")
+    fields.map(field => {
+      if (field == 'country') {
+        const result = countries.filter(country =>
+          country.country_full_name_en.toLowerCase().includes(customer[field].toLowerCase())
+        );
+        return result[0]?.country_code || ""
+      }
+
+      if (field == 'is_thai_nationality' && customer.nationality == 'THAI') {
+        return "T"
+      } else if (field == 'is_thai_nationality' && customer.nationality != 'THAI') {
+        return "F"
+      }
+
+      if (field == 'opening_account_date') {
+        return customer[field].substring(0, 10);
+      }
+
+      if (field == 'report_date') {
+        return '2025-03-10'
+      }
+
+      if (field == 'nationality') {
+        const result = nationalities.filter(nationality =>
+          nationality.nationality_name_en.toLowerCase().includes(customer[field].toLowerCase())
+        );
+        return result[0]?.nationality_code || ""
+      }
+
+      if (field == 'table_id' || field == 'intermediary_id') {
+        return '0105561177671'
+      }
+
+      if (field == 'is_update') {
+        return 'F'
+      }
+
+      if (field == 'name_title') {
+        const result = titles.filter(title =>
+          title.title_name_en.toLowerCase().includes(customer[field].toLowerCase())
+        );
+        return result[0]?.title_code || "-"
+      }
+
+      if (field == 'customer_code_amlo') {
+        return '-'
+      }
+
+      if(field == 'customer_id'){
+        return customer.tax_id
+      }
+
+      return customer[field] || "-";
+    }).join("|")
   );
+
+  console.log(processedData);
   return processedData
 }
 
@@ -179,35 +253,35 @@ function processOutStanding(customers, fields) {
 
   const processedData = customers.map(customer =>
     fields.map(field => {
-      if(field === 'da_asset_short_name'){
+      if (field === 'da_asset_short_name') {
         return da_asset_short_name
       }
 
-      if(field === 'da_asset_id'){
+      if (field === 'da_asset_id') {
         return da_asset_id
       }
 
-      if(field === 'is_digital_asset_outstanding'){
+      if (field === 'is_digital_asset_outstanding') {
         return is_digital_asset_outstanding
       }
 
-      if(field === 'fiat_asset_id'){
+      if (field === 'fiat_asset_id') {
         return fiat_asset_id
       }
 
-      if(field === 'da_wallet_address'){
+      if (field === 'da_wallet_address') {
         return da_wallet_address
       }
 
-      if(field === 'fiat_quantity'){
+      if (field === 'fiat_quantity') {
         return fiat_quantity
       }
 
-      if(field === 'da_asset_isin'){
+      if (field === 'da_asset_isin') {
         return da_asset_isin
       }
 
-      if(field === 'customer_code_amlo'){
+      if (field === 'customer_code_amlo') {
         return customer_code_amlo
       }
 
@@ -265,8 +339,8 @@ const yyyymmdd = 20250310;
 var report_date = "2025-03-10"
 
 const templates = [
-  // "ICOPortal_DA_CusData_{dbdNo}_{assetId}_{yyyymmdd}.csv",
-  "ICOPortal_DA_CusOutstanding_{dbdNo}_{assetId}_{yyyymmdd}.csv",
+   "ICOPortal_DA_CusData_{dbdNo}_{assetId}_{yyyymmdd}.csv",
+  // "ICOPortal_DA_CusOutstanding_{dbdNo}_{assetId}_{yyyymmdd}.csv",
   //"ICOPortal_DA_CusWallet_{dbdNo}_{assetId}_{yyyymmdd}.csv",
   //"ICOPortal_DA_Identification_{dbdNo}_{assetId}_{yyyymmdd}.csv",
   //"ICOPortal_DA_ProfilePortal_{dbdNo}_{assetId}_{yyyymmdd}.csv"
